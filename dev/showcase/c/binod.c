7e2e5c8a718a6eb1be185b6c80be299861de337a
Ashut0sh-M <113246779+Ashut0sh-M@users.noreply.github.com>
2022-09-18 14:23:51 +0530

    Add files via upload

---
+++ dev/showcase/c/binod.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  printf("Binod.\n");
+  return 0;
+}
---
+++ dev/showcase/cpp/binod.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Binod.\n";
+  return 0;
+}
